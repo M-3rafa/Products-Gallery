c96346f303704d036e7b34b278b75e121b8509c1
3rafa <official3rafa@gmail.com>
2025-07-28 02:44:19 +0300

    updatessr

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,9 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'detalils/:id',
+    renderMode: RenderMode.Client,
+  },
+
+  { path: '**', renderMode: RenderMode.Prerender },
 ];
